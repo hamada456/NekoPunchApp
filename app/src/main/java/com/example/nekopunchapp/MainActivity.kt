395a373d494cb6e27b24edf90c52d1a1a80f2f79
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-12-03 16:06:01 +0900

    完成

--- app/src/main/java/com/example/nekopunchapp/MainActivity.kt
+++ app/src/main/java/com/example/nekopunchapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.nekopunchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //画面タッチイベント
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var iv :ImageView = findViewById(R.id.iv)
+
+        when(event?.action){
+            //画面をタッチした時、画像をcat02に変更する
+            MotionEvent.ACTION_DOWN -> iv.setImageResource(R.drawable.cat02)
+            //画面をタッチした時、画像をcat02に変更する
+            MotionEvent.ACTION_MOVE -> iv.setImageResource(R.drawable.cat03)
+            //画面をタッチした時、画像をcat02に変更する
+            MotionEvent.ACTION_UP   -> iv.setImageResource(R.drawable.cat01)
+        }
+        return super.onTouchEvent(event)
     }
 }
